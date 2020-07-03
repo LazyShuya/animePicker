628f8b34425d717d39fb07d8d4ec7da7f84f5a6c
LazyShuya <yittrium3@gmail.com>
2020-07-03 20:32:24 +0530

    checking

--- recs_database.py
+++ recs_database.py
@@ -93,29 +93,3 @@ def random_function():
             list_for_cache.append(anime_to_display)
 
 
-
-#     return
-# class Recommend:
-#     def __init__(self, web_link, anime_name):
-#         self.recs = []
-#         self.anime = anime_name
-#         self.d_main = pd.read_csv('test.csv')
-
-#     def D_Base(self, list_names):
-#         for name in list_names:
-#             df2 = {"anime":self.anime, "recs":name}
-#             frame = pd.DataFrame([df2])
-#             self.d_main = self.d_main.append(frame, ignore_index=True)
-        
-#         self.d_main.drop(self.d_main.filter(regex="Unname"),axis=1, inplace=True)
-#         self.d_main.to_csv('test.csv')
-
-#     def Find_Recs(self):
-#         self.stuff = self.soup.find_all(style="margin-bottom: 2px;")
-#         for text in self.stuff:
-#             self.recs.append(text.find('strong').get_text())
-#         self.D_Base(self.recs)
-
-# con = Recommend('https://myanimelist.net/anime/11843/Danshi_Koukousei_no_Nichijou/userrecs', 'Danshi Koukousei no Nichijou')
-# df = con.cbase()
-# print(df.anime.unique())
